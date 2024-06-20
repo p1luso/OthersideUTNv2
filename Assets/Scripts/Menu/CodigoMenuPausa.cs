b80e697ad7d22f711e8f285272eeba034a9b5bb4
PerpetuaF <perpetuafranck@gmail.com>
2024-06-20 19:13:53 -0300

    Final Final Build

--- Assets/Scripts/Menu/CodigoMenuPausa.cs
+++ Assets/Scripts/Menu/CodigoMenuPausa.cs
@@ -26,7 +26,7 @@ public class CodigoMenuPausa : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             if (!_interactableText.isShowing)
             {
